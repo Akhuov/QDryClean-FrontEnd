b1611e83d0cc0d4db19a7e16a6cfa6006bc18cbc
Akbarxon <Akbarxon@DESKTOP-UI60GGL>
2025-09-16 16:21:10 +0500

    small fix authApi (edit post name)

--- QDryClean-FrontEnd/src/features/auth/api/authApi.js
+++ QDryClean-FrontEnd/src/features/auth/api/authApi.js
@@ -2,6 +2,6 @@ import axiosInstance from "@/shared/api/axiosInstance";
 
 // Функция логина
 export const loginUser = async (credentials) => {
-  const response = await axiosInstance.post("/Auth/Login", credentials);
+  const response = await axiosInstance.post("/Auth", credentials);
   return response.data; // { token, user }
 };
